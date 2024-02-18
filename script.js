db8ed978fd53c849960f05922cec893ae8d299e3
MASUMHD <mrmasum2k4@gmail.com>
2024-02-18 18:59:55 +0600

    first commit all code

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-alert()
+
+
+const allBtn = document.getElementsByClassName("done");
+let count = 0;
+for (const all of allBtn) {
+    all.addEventListener("click", function (e) {
+        count++;
+
+
+        document.getElementById('A1').addEventListener('click',function(){
+            const commentBox = document.getElementById('new-comment');
+            const newComment =commentBox.value;
+
+        })
+
+        setInnerText("seat-count", count);
+    });
+}
+
+function setInnerText(id, value) {
+    document.getElementById(id).innerText = value;
+}

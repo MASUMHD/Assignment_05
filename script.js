93f8480e0a587314f8547d8faf41a98ae42a67ba
MASUMHD <mrmasum2k4@gmail.com>
2024-02-18 21:28:00 +0600

    all code push

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 
 
-const allBtn = document.getElementsByClassName("done");
+const allBtn = document.getElementsByClassName("btn");
 let count = 0;
+let counts = 40;
 for (const all of allBtn) {
     all.addEventListener("click", function (e) {
         count++;
+        counts--;
 
+        // document.getElementById('A1').addEventListener('click',function(){
+        //     const commentBox = document.getElementById('new-comment');
+        //     const newComment =commentBox.value;
+        //     const p = document.createElement('p');
+        //     p.innerText = newComment;
 
-        document.getElementById('A1').addEventListener('click',function(){
-            const commentBox = document.getElementById('new-comment');
-            const newComment =commentBox.value;
-
-        })
+        // })
 
         setInnerText("seat-count", count);
+        setInnerText("Seats-left", counts);
     });
 }
 

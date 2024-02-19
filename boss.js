83fe53e6bd64ae624816ba86bed0f7053ecf607e
MASUMHD <mrmasum2k4@gmail.com>
2024-02-19 17:20:05 +0600

    all code push

--- boss.js
+++ boss.js
@@ -1,4 +1,3 @@
-
 const allBtn = document.getElementsByClassName("seat");
 let count = 0;
 let counts = 40;
@@ -9,6 +8,7 @@ for (const all of allBtn) {
     all.addEventListener("click", function (e) {
         if (!all.classList.contains("clicked")) {
             if (count < 4) {
+                addSeat(this.value);
                 count++;
                 counts--;
 
@@ -18,6 +18,7 @@ for (const all of allBtn) {
                 setInnerText("Seats-left", counts);
                 setInnerText("total-price", totalPrice);
                 setInnerText("grand-total", grandTotal);
+                setInnerText("grand-total", totalPrice);
 
                 all.classList.add("clicked");
                 all.style.backgroundColor = "#1DD100";
@@ -32,21 +33,48 @@ function setInnerText(id, value) {
     document.getElementById(id).innerText = value;
 }
 
+
+
+// add seat and price.........................>>>>>>>>
+
+
+
+const seatsSelected = [];
+
+function addSeat(seatId) {
+    seatsSelected.push(seatId);
+    
+    document.getElementById("seatsSelected").innerHTML = '';
+    let html = '';
+    seatsSelected.forEach(seat => {        
+        html += '<p class="flex justify-between"><span>'+ seat +'</span><span>Economoy</span><span>550</span> </p>';        
+    })
+
+    document.getElementById("seatsSelected").innerHTML = html;
+}
+
+
+// calculate grand total and discount amount...............>>>>>>>>
+
+
+
 function calculateGrandTotal() {
     const couponCode = document.getElementById("input").value;
 
-    let discountPercentage = 0;
+    let discount = 0;
 
     if (couponCode === "NEW15") {
-        discountPercentage = 15;
+        discount = 15;
     } else if (couponCode === "Couple 20") {
-        discountPercentage = 20;
+        discount = 20;
     }
 
-    const discountAmount = (totalPrice * discountPercentage) / 100;
+    const discountAmount = (totalPrice * discount) / 100;
     grandTotal = totalPrice - discountAmount;
 
     setInnerText("grand-total", grandTotal);
+
+
 }
 
 document.getElementById("apply-btn").addEventListener("click", function () {
@@ -55,16 +83,22 @@ document.getElementById("apply-btn").addEventListener("click", function () {
     document.getElementById("apply-btn").style.display = "none";
 });
 
+
+
+// from section here..................................................>>>>>>>
+
+
+
 document.getElementById("next-btn").addEventListener("click", function () {
     const passengerName = document.getElementById("passenger-name").value;
     const phoneNumber = document.getElementById("phone-number").value;
     const emailId = document.getElementById("email-id").value;
 
     if (passengerName && phoneNumber && emailId) {
-        // alert("Congratulations! Your booking is confirmed.");
+
         document.getElementById("myModal").showModal();
+
     } else {
         alert("Please fill in all the required fields.");
     }
 });
-
